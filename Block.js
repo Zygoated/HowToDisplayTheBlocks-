ff575be02b3c73d7af4f2da092a9cda9e65872ab
Zygoated <75553331+Zygoated@users.noreply.github.com>
2021-08-05 15:34:28 -0600

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,27 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image= loadImage("sprites/block.png")
+      this.Visiblity = 255;
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed > 3){
+        this.Visiblity= this.Visiblity
+       }
+       else{
+         World.remove(world, this.body);
+         push();
+         this.Visiblity = this.Visiblity - 5;
+         tint(255,this.Visiblity);
+         image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+         pop();
+       }
+    }
+}
